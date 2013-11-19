db11607861d6757d599205a6221fa965b4bc570a
Einar Thor <einarthor13@ru.is>
2013-11-19 13:58:44 +0000

    UserRegistration bætt við

---
+++ app/is/ru/honn/rustagram/domain/UserRegistration.java
@@ -0,0 +1,32 @@
+package is.ru.honn.rustagram.domain;
+
+import java.util.Date;
+
+public class UserRegistration extends User
+{
+    protected String repeatPassword;
+    public UserRegistration()
+    {
+    }
+    public UserRegistration(int id, String name, String username, String password,
+                            String email, String repeatPassword, Gender gender, Date registered)
+    {
+        //public User(int id, String username, String password, String displayName, String email, Gender gender, Date registered)
+        super(id, name, username, password, email, gender, registered);
+        this.repeatPassword = repeatPassword;
+    }
+    public UserRegistration(String name, String username, String password,
+                            String email, String repeatPassword, Gender gender)
+    {
+        super( name, username, password, email, gender);
+        this.repeatPassword = repeatPassword;
+    }
+    public String getRepeatPassword()
+    {
+        return repeatPassword;
+    }
+    public void setRepeatPassword(String repeatPassword)
+    {
+        this.repeatPassword = repeatPassword;
+    }
+}
